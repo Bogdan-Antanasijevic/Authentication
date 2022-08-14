92f470513261eca9f199928ade5ad509e3efc271
Bogdan <b.antanasijevic026@gmail.com>
2022-08-15 00:44:11 +0200

    login and register css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css';
+import Dashboard from './pages/Dashboard/Dashboard';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div>
-      <h1>My app</h1>
-    </div>
+    <>
+      <Router>
+        <div className='container'>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Dashboard/>} />
+            <Route path='/login' element={<Login/>} />
+            <Route path='/register' element={<Register/>} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
