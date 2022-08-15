063b85dc9f92d62eac82fdaa4dd4a62fdf2d0b35
Bogdan <b.antanasijevic026@gmail.com>
2022-08-16 01:16:50 +0200

    login and register finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 import './App.css';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
-import Header from './components/Header';
+import Header from './components/Header/Header';
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
           </Routes>
         </div>
       </Router>
+      <ToastContainer/>
     </>
   );
 }
--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import { FaSignInAlt } from 'react-icons/fa'
 import './Login.scss'
+import {useSelector, useDispatch} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+import {toast} from 'react-toastify'
+import {login, reset} from '../../features/auth/authSlice'
+import Spinner from '../../components/Spinner/Spinner'
 
 function Login() {
   const [fomrData, setFormData] = useState({
@@ -10,6 +15,24 @@ function Login() {
   })
 
   const { email, password } = fomrData
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const {user, isLoading, isError, isSuccess, message} = useSelector((state)=> state.auth)
+
+  useEffect(()=>{
+    if(isError){
+      toast.error(message)
+    }
+    if(isSuccess || user){
+      navigate('/')
+       
+    }
+
+    dispatch(reset())
+
+  },[user,isError,isSuccess,message,navigate,dispatch])
 
   const onChange = (e) => {
     setFormData((prevState)=>({
@@ -20,9 +43,18 @@ function Login() {
 
   const onSubmit = (e) => {
     e.preventDefault()
+
+    const userData = {
+      email,
+      password
+    }
+
+    dispatch(login(userData))
   }
 
-
+  if(isLoading){
+    return <Spinner/>
+  }
   return (
     <>
       <section className='heading'>
